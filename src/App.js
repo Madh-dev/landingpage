1e1e711592e03dc41f96c36d41c13bb18b18b2f2
AbdulMuheez <Aleem.abdulMuheez@raoatech.com>
2023-05-22 14:27:39 +0100

    landing page sorted out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setShowButton(screenWidth <= 1024);
+    };
+
+    // Call the handleResize function initially to set the initial state based on the current screen size
+    handleResize();
+
+    // Add a resize event listener to update the state when the screen size changes
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='imagePart'><img src='/image.svg' alt='herobanner' /></div>
+        <div className='textPart'>
+          <h4>Imagine if <br />
+            <span className='snap'>Snapchat</span><br />
+            had events.</h4>
+          <p>Easily host and share events with your friends across any social media</p>
+          {!showButton &&<button>🎉Create my event</button>}
+        </div>
+        {showButton &&<button>🎉Create my event</button>}
+      </div>
     </div>
   );
 }
